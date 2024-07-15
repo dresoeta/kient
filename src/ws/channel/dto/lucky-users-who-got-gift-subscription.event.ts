00e249b1e12e53f0b9d19db9cf0c77cb83919cc6
Dresoeta <dresoeta@gmail.com>
2024-07-15 17:43:11 +0700

    Update package version and simplify event model
    Bump the version number in `package.json` from 1.0.1-21 to 1.0.1-22. The 'lucky-users-who-got-gift-subscription.event.ts' file has been modified to simplify the event model, removing excessive "chatroom" details while adding 'vod_enabled' and 'subscription_enabled' properties.

--- src/ws/channel/dto/lucky-users-who-got-gift-subscription.event.ts
+++ src/ws/channel/dto/lucky-users-who-got-gift-subscription.event.ts
@@ -6,23 +6,9 @@ export interface LuckyUsersWhoGotGiftSubscriptionsEvent {
     is_banned: boolean
     playback_url: string
     name_updated_at: unknown
+    vod_enabled: boolean
+    subscription_enabled: boolean
     can_host: boolean
-    chatroom: {
-      id: number
-      chatable_type: string
-      channel_id: number
-      created_at: Date
-      updated_at: Date
-      chat_mode_old: string
-      chat_mode: string
-      slow_mode: boolean
-      chatable_id: number
-      followers_mode: boolean
-      subscribers_mode: boolean
-      emotes_mode: boolean
-      message_interval: number
-      following_min_duration: number
-    }
   }
   usernames: string[]
   gifter_username: string
